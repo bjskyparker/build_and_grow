931198802df7c251eab31bdd9a17d89d8fd32bf0
Brian Parker <parkerbrian44@gmail.com>
2021-02-18 08:58:14 -0500

    trying to get the app to work with redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import BasicTable from './components';
 import Forms from './components/Forms';
+import { useSelector } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
 
 
 function App() {
+  const rootReducer = useSelector(state => state.rootReducer)
+
+
   return (
     <div className="App">
       <p>
